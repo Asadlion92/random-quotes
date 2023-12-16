8398e2b833cfc8bca65ffe76463ae65f168f6dbe
Jafar Asad <rafajdasa@gmail.com>
2023-12-16 18:34:06 +0300

    created quote generator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Quote from './Quote/Quote';
+
+function App() {
+
+  return (
+    <div>
+      <Quote />
+    </div>
+  )
+}
+
+export default App
